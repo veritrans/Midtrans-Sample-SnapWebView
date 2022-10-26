2b1236dba4e5da1beb19947f47770d9fe5e1affa
Ferdian Julianto <Juliantoferdian24@gmail.com>
2022-10-26 16:34:22 +0700

    feat: update UI in input_url_screen

--- lib/input_url_screen.dart
+++ lib/input_url_screen.dart
@@ -15,35 +15,48 @@ class InputUrlState extends State<InputUrlScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _url,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Put your SNAP link (optional)'),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              const Image(
+                  image: AssetImage('assets/midtrans-logo.png'),
+                  width: 200,
+                  height: 42),
+              const SizedBox(height: 10),
+              const Text('SNAP on WebView', style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: TextField(
+                  controller: _url,
+                  decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF0A2852))),
+                      labelStyle: TextStyle(color: Color(0xFF0A2852)),
+                      labelText: 'Put your SNAP link (optional)'),
+                ),
               ),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  String url;
-                  if (_url.text == "") {
-                    url = "https://sample-demo-dot-midtrans-support-tools.et.r.appspot.com/snap-redirect/";
-                  } else {
-                    url = _url.text;
-                  }
-                  Navigator.of(context).pushNamed(
-                    SnapWebViewScreen.routeName,
-                    arguments: {
-                      'url': url,
-                    },
-                  );
-                },
-                child: const Text('Go Next Page'))
-          ],
+              ElevatedButton(
+                  onPressed: () {
+                    String url;
+                    if (_url.text == "") {
+                      url =
+                          "https://sample-demo-dot-midtrans-support-tools.et.r.appspot.com/snap-redirect/";
+                    } else {
+                      url = _url.text;
+                    }
+                    Navigator.of(context).pushNamed(
+                      SnapWebViewScreen.routeName,
+                      arguments: {
+                        'url': url,
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF0A2852)),
+                  child: const Text('Go Next Page'))
+            ],
+          ),
         ),
       ),
     );
