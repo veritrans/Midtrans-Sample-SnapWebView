750f86b4458fdc0b6c1b80c05ebdd6c8de42bcb3
Ferdian Julianto <Juliantoferdian24@gmail.com>
2022-10-26 11:32:49 +0700

    feat: Adding page load events

--- lib/snap_web_view_screen.dart
+++ lib/snap_web_view_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-
 class SnapWebViewScreen extends StatefulWidget {
   static const routeName = '/snap-webview';
 
@@ -12,16 +11,42 @@ class SnapWebViewScreen extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<SnapWebViewScreen> {
+  var loadingPercentage = 0;
+
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final url = routeArgs['url'];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
       ),
-      body: WebView(
-        initialUrl: url,
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: url,
+            onPageStarted: (url) {
+              setState(() {
+                loadingPercentage = 0;
+              });
+            },
+            onProgress: (progress) {
+              setState(() {
+                loadingPercentage = progress;
+              });
+            },
+            onPageFinished: (url) {
+              setState(() {
+                loadingPercentage = 100;
+              });
+            },
+          ),
+          if (loadingPercentage < 100)
+            LinearProgressIndicator(
+              value: loadingPercentage / 100.0,
+            ),
+        ],
       ),
     );
   }
